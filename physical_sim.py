5f11ae899f0c053e7f8f4e1e509b0c16ada7cf6f
dylanalexander <dylan@postriver.com>
2019-03-26 17:11:59 -0500

    Added code for gravitational forces.

--- physical_sim.py
+++ physical_sim.py
@@ -11,8 +11,8 @@ number_particles = 3
 state_particle_length = 1 + 3 + 3 #length one particle takes in the state vector that describes the system
 state_len = number_particles * state_particle_length
 
-mean_mass   = 1000.
-mass_spread = 15.
+mean_mass   = 10000.
+mass_spread = 5000.
 mean_x   = 0.0
 x_spread = 2.
 mean_y   = 0.0
@@ -28,6 +28,7 @@ v_z_spread = 100.0
 
 def mk_particle():
     #returns a random particle state according to some specification
+    #particles have no charge and are point masses
 
     return np.random.normal(loc=[mean_mass,mean_x,mean_y,mean_z,mean_v_x,mean_v_y,mean_v_z],
         scale=[mass_spread,x_spread,y_spread,z_spread,v_x_spread,v_y_spread,v_z_spread,])
@@ -38,10 +39,37 @@ def mk_rand_state():
         #state has structure: [m1 x1 ... v_x1 ... particle2 ...]
         #get particle
         p = mk_particle()
-        while p[0] >= 0.:
+        #print("Made particle {}.".format(p))
+        while p[0] < 0.:
             p = mk_particle() #ensures mass is positive
-        result_state[i*number_particles:(i+1)*number_particles] = p #set slice of array
+
+        result_state[i*number_particles: state_particle_length + i*number_particles] = p #set slice of array
     return result_state
+
+def get_gravitational_forces(state):
+    forces = np.zeros(3*number_particles)
+    #Each spatial dimension gets a component of the forces for each particle
+    #The forces are total and work of the softened gravitational force
+    #F = Gm1m2/(r^2+eps^2)^3/2*r
+
+    #should have (n-1)(n-2) force terms to compute total where n is num particles
+    for subject in range(number_particles):
+        for agent in range(subject+1, number_particles):
+            #agent does force on subject
+            #First [0] index at loc is the mass
+            r = state[subject+1:subject+1+3] - state[agent+1:agent+1+3] #radial between particles
+            f = G*state[subject]*state[agent] * r /np.power(np.dot(r,r) + epsilon**2,3/2)
+            forces[subject:subject + 3] += f
+            forces[agent:agent + 3] += - f
+
+    return forces
+
+
+def state_advance(state):
+    #Advances the state 1 leapfrog time step, returning the new state.
+    new_state = np.array(state, copy=True)
+    #This uses the yoshida integrator method
+    f_i = get_gravitational_forces(state) #Gets the gravitational forces on the current state
 
 
 
---
+++ testing.py
@@ -0,0 +1,22 @@
+
+from pprint import pprint
+from physical_sim import *
+
+#This is where the bulk of the testing code should go
+
+
+
+def test_batch_0():
+    #First test to see if interface works:
+    pprint(mk_rand_state())
+
+
+
+def main():
+    test_batch_0()
+
+
+
+if __name__ == "__main__":
+    main()
+
